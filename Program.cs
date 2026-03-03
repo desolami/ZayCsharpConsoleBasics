c5cb98652218020dfbb39df5483729d12f9aa41c
desolami <99566510+desolami@users.noreply.github.com>
2026-03-03 03:29:03 +0100

    Add program.cs

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("What is your name?");
+string name = Console.ReadLine();
+
+Console.WriteLine("Hello " + name + "! Welcome to C#.");
+Console.WriteLine("How old are you?");
+int age = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Next year, you will be " + (age + 1));
+Console.WriteLine("Where do you stay?");
+string address = Console.ReadLine();
+Console.WriteLine("i stay around " + address + "in  Lagos, Nigeria");
